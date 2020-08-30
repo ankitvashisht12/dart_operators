816dcad177ed303d8c9a5e173b5dc70f71bdadfb
Ankit Vashisht <ankitvashisht12@gmail.com>
2020-08-30 11:12:35 +0530

    add dart function for cascade understanding

--- lib/operators/cascade_opt.dart
+++ lib/operators/cascade_opt.dart
@@ -1,1 +1,24 @@
+import 'package:flutter/material.dart';
 
+List<int> withoutCascade() {
+  final list = [5, 1, 4, 3, 2];
+  //sort list
+  list.sort();
+  // remove last element
+  list.removeLast();
+  return list;
+}
+
+List<int> withCascade() {
+  final list = [5, 1, 4, 3, 2];
+  return list
+    ..sort()
+    ..removeLast();
+}
+
+class CascadeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
